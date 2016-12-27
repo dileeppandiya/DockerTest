4375350bb0b938b1903f094223d41754843634d6
DILEEP KUMAR PANDIYA <DILEEP.PANDIYA01@ad.Infosys.com>
2016-12-26 23:00:42 -0600

    message

---
+++ src/DockerTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this test is sucessful go to sleep");
+            Console.ReadLine();
+        }
+    }
+}
